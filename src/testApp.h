c1e8b6d6b7b490db5015e58d007ca0454dd86632
tokujin <Nori@Norihitos-MacBook-Pro.local>
2013-11-10 08:37:24 -0500

    error
    error: perhaps related to syphon

--- src/testApp.h
+++ src/testApp.h
@@ -5,7 +5,13 @@
 #include "ofxUI.h"
 
 #include "particle.h"
+#include "ParticleN.h"
+#include "ParticleJ.h"
 #include "vectorField.h"
+#include "fft.h"
+#include "FFTOctaveAnalyzer.h"
+
+#define BUFFER_SIZE 512
 
 //#define _USE_LIVE_VIDEO
 
@@ -25,7 +31,9 @@ public:
 	void windowResized(int w, int h);
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
-	
+    
+	//---------------------------
+    //Matt's Animation
 	// particle & vector field-related
 	void addParticle(float x, float y, float vx, float vy);
 	vector <particle> particles;
@@ -74,10 +82,61 @@ public:
 	bool increaseBlurAndDim;
 	float imageDiffThreshold;
 	float imageDiffBlur;
-	
-	// gui related
+
+    //Gui related
 	ofxUICanvas *gui;
 	void setupGUI();
 	void guiEvent(ofxUIEventArgs &e);
+
+	//---------------------------
+    //Nori's Animation
+    //particle, vector field related
+	vector <ParticleN> particlesN;
+	vectorField VF;
 	
+	float prevMouseX;
+	float prevMouseY;
+    
+    //fft related
+//    void audioReceived(float* input, int bufferSize, int nChannels);
+    FFTOctaveAnalyzer FFTanalyzer;
+    float left[BUFFER_SIZE];
+    float right[BUFFER_SIZE];
+    fft		myfft;
+    float magnitude[BUFFER_SIZE];
+    float phase[BUFFER_SIZE];
+    float power[BUFFER_SIZE];
+    float freq[BUFFER_SIZE/2];
+    
+    //Gui related
+    ofxUICanvas *guiN;
+    
+    //Fbo related
+    ofFbo  mFboN;
+    ofFbo  pallette;
+    
+    //program related
+    float friction;
+    float radius;
+    bool resetButton;
+    bool clearButton;
+    bool randomVF;
+    bool startNori;
+    
+    float soundVol, soundVolMax; //sound volume
+    float soundVolL, soundVolLMax;
+    float soundVolH, soundVolHMax; //average of magnitude in lower bands, in higher bands
+    int alpha;
+    float nradius;
+    float rotate;
+    bool colorchange;
+    
+    //color palette
+    float blue, red;
+    float r1,g1,b1, r2,g2,b2, r3,g3,b3;
+    
+
+    
+    
+    
 };
